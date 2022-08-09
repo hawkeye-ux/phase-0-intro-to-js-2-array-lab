a325913001445866152511b200c617681834ea5a
Evans <evans.kibet@student.moringaschool.com>
2022-08-09 19:22:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    let catsNewArray =  [...cats, name];
+    return catsNewArray;
+}
+
+function prependCat(name) {
+    let catsNewArray =  [name, ...cats];
+    return catsNewArray;
+}
+
+function removeLastCat() {
+    let catsNewArray = cats.slice(0, -1);
+    return catsNewArray;
+}
+
+function removeFirstCat() {
+    let catsNewArray = cats.slice(1);
+    return catsNewArray;
+}
